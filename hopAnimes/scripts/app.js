684a3cca0686a7a206831ecd5ac2383536d0776a
Hermenegildo Da Cruz <138516502+HermenegildoDaCruz@users.noreply.github.com>
2025-03-26 12:59:41 +0100

    Add files via upload

---
+++ hopAnimes/scripts/app.js
@@ -0,0 +1,64 @@
+//Fetch animes
+const fetchAnimes = async () => {
+    const response = await fetch('https://api.jikan.moe/v4/anime') // change fetch url to get all animes
+    if (response.status === 200){
+        return await response.json()
+    } else {
+        throw new Error('Unable to fetch Animes')
+    }
+}
+
+//Render animes
+const renderAnimes = (animes) => {
+    document.querySelector('.animes').innerHTML = ""       
+    animes.forEach(anime => {
+        const imageUrl = anime.images.webp.image_url 
+        const title = anime.title
+        const dateISO =  `${anime.aired.from}`
+        const date = new Date(dateISO)
+        const formatMonth = (date.getUTCMonth() + 1) < 10 ? `0${date.getUTCMonth()}`:date.getUTCMonth()
+
+
+        document.querySelector('.animes').innerHTML += `
+        <div class='anime'>
+        <a href="${anime.url}" target = '_blank'><img src="${imageUrl}" alt=""></a>
+        <div class="info">
+            <a href="${anime.url}" target = '_blank'><h2>${title}</h2></a>
+            <div>
+                <span>Release: ${date.getUTCDate()}-${formatMonth}-${date.getUTCFullYear()}</span>
+                <a href="${anime.trailer.url}" target = "_blank" class='seeTrailer'>Trailer: <button class="seeTrailer"><img src="play-svgrepo-com.svg" alt="play" class="play"></button></a>
+            </div>
+        </div>
+        </div>
+        
+        `      
+    });
+}
+
+//Render all animes
+try {
+    fetchAnimes().then((animes) => {
+        renderAnimes(animes.data)
+    })
+} catch (error) {
+    console.log(error)
+    alert(error)
+}
+
+//Search
+document.querySelector('#search').addEventListener('input', (e) => {
+    try {
+        fetchAnimes().then((animes) => {
+            const filteredAnimes = animes.data.filter((anime)=>{
+                return anime.title.toLowerCase().includes(e.target.value.toLowerCase())
+            })
+
+            if(filteredAnimes.length > 0){
+                renderAnimes(filteredAnimes)
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        alert(error)
+    }
+})
